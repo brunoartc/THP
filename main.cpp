d4cfe0c3b64bbce2d3088ff3aedf70f749133688
Bruno Arthur <brunoartc@yahoo.com>
2020-02-12 20:16:47 -0300

    Backbone add main & Makefile add tests

---
+++ main.cpp
@@ -0,0 +1,83 @@
+#include <iostream> 
+#include <regex> 
+#include <string.h> 
+using namespace std; 
+
+#ifndef _TESTS
+int main(int argc, char const *argv[]){
+
+    return 0;
+}
+#endif
+
+
+
+
+
+
+stack<int> reverse_polish(string s1){
+    regex numbers(R"([0-9]+)");
+    regex operators(R"([+\-*/%])");
+
+
+    sregex_iterator iter_numbers(s1.begin(), s1.end(), numbers);
+
+    sregex_iterator iter_operators(s1.begin(), s1.end(), operators);
+    sregex_iterator end;
+
+    stack <int> number_stack;
+
+
+    while(iter_numbers != end)
+    {
+
+        for(unsigned i = 0; i < iter_numbers->size(); ++i)
+        {
+            number_stack.push(stoi((*iter_numbers)[i]));
+
+        }
+        ++iter_numbers;
+    }
+
+
+
+
+    while(iter_operators != end)
+    {
+        int x, y;
+        for(unsigned i = 0; i < iter_operators->size(); ++i)
+        {
+            string result_string = (*iter_operators)[i];
+            switch (result_string[0])
+            {
+            case '+':
+                x = number_stack.top();
+                number_stack.pop();
+                y = number_stack.top();
+                number_stack.pop();
+
+                number_stack.push(x+y);
+                break;
+            case '-':
+                x = number_stack.top();
+                number_stack.pop();
+                y = number_stack.top();
+                number_stack.pop();
+
+                number_stack.push(x-y);
+                break;
+            
+            default:
+                break;
+            }
+
+        }
+        ++iter_operators;
+    }
+
+    if (number_stack.size() != 1){
+        cout << "WARN:\tInput Error";
+    }
+    
+    return number_stack;
+}
---
+++ tests.cpp
@@ -0,0 +1,12 @@
+#define _TESTS
+#include "main.cpp"
+
+
+int main(int argc, char const *argv[])
+{
+    if (reverse_polish("1+1 +1 +1 +1").size() != 1) return -1;
+    if (reverse_polish("1+1 +1 +1 +1").top() != 5) return -1;
+    if (reverse_polish("1+1 +1 +1 +1 - 1").top() != 4) return -1;
+    if (reverse_polish("1+1 +1 +1 +1 1").size() != 2) return -1;
+    return 0;
+}
